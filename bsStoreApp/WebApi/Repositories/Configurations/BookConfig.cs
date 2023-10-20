276ab9af412c4307ec9c5b718ab1e7034ac7347b
AtakanTurgut <77547962+AtakanTurgut@users.noreply.github.com>
2023-10-20 21:23:00 +0300

    ef-core has been implemented.

---
+++ bsStoreApp/WebApi/Repositories/Configurations/BookConfig.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using WebApi.Models;
+
+namespace WebApi.Repositories.Configurations
+{
+    public class BookConfig : IEntityTypeConfiguration<Book>
+    {
+        public void Configure(EntityTypeBuilder<Book> builder)
+        {
+            builder.HasData(
+                    new Book { Id = 1, Title = "ASP.NET Framework", Price = 120 },
+                    new Book { Id = 2, Title = "MVC Pattern", Price = 150 },
+                    new Book { Id = 3, Title = "Javascript", Price = 65 }
+                );
+        }
+    }
+}
