9c8c8b6413145c3d5a3896428276aaeb5dc68641
AtakanTurgut <77547962+AtakanTurgut@users.noreply.github.com>
2023-11-08 23:19:43 +0300

    Projects have been updated.

---
+++ basics/HelloWebApi/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using HelloWebApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelloWebApi.Controllers
+{
+    [ApiController]
+    [Route("home")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        /*public ResponseModel GetMessage()
+        {
+            // return "Hello ASP.NET Core Web API";
+
+            return new ResponseModel()
+            {
+                HttpStatus = 200,
+                Message = "Hello ASP.NET Core Web API"
+            };
+        }
+        */
+
+        public IActionResult GetMessage()
+        {
+            var result = new ResponseModel()
+            {
+                HttpStatus = 200,
+                Message = "Hello ASP.NET Core Web API"
+            };
+
+            return Ok(result);
+        }
+    }
+}
